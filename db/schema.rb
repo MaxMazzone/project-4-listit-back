89d9c956fa9667937bdba88c9fbf5cd2a620f50e
stgeorge3 <amaxmazzone@gmail.com>
2017-11-01 10:50:26 -0400

    Migrates addition of foreign key to apartments resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101143432) do
+ActiveRecord::Schema.define(version: 20171101144526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20171101143432) do
     t.boolean "pets_allowed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_apartments_on_user_id"
   end
 
   create_table "examples", force: :cascade do |t|
@@ -44,5 +46,6 @@ ActiveRecord::Schema.define(version: 20171101143432) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "apartments", "users"
   add_foreign_key "examples", "users"
 end
