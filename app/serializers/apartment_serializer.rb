1b32976353351b7b9411ba29d1c590bd8237c7c6
stgeorge3 <amaxmazzone@gmail.com>
2017-11-01 18:57:41 -0400

    Add editable property to apartments resource
    Editable property shows if user can edit this resources

--- app/serializers/apartment_serializer.rb
+++ app/serializers/apartment_serializer.rb
@@ -1,3 +1,7 @@
 class ApartmentSerializer < ActiveModel::Serializer
-  attributes :id, :address, :rent, :bedrooms, :bathrooms, :description, :pets_allowed
+  attributes :id, :address, :rent, :bedrooms, :bathrooms, :description, :pets_allowed, :editable
+
+  def editable
+    scope == object.user
+  end
 end
